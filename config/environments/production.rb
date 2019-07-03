9fcea4ea6ba0fdb9959107b64ad2b621f35e8fac
Isabella DiMambro <belladimambro@gmail.com>
2019-07-03 01:43:02 +0000

    syntax correction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
-config.action_mailer.default_url_options = { host: 'grammable-dimambro.herokuapp.com' }
-  grammable-dimambro.herokuapp.com
+config.action_mailer.default_url_options = { host: 'grammable2-dimambro.herokuapp.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
